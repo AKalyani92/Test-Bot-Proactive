9768c538fd3cb0a786433445f7a334424c523559
Aditi Kalyani <aditikalyani92@gmail.com>
2017-08-18 15:27:12 +0530

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var request = require('request');
+var CronJob = require('cron').CronJob;
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -12,16 +13,57 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: '316a82f0-dfb8-4f94-8a43-3ef6b572d99a', appPassword: 'yuDtZnm8Rjvhp0Ciuy04HGO' });
+({ appId: 'e2e4a314-0201-404d-964a-2b665fffba1a', appPassword: 'yEph0VAaRnmS7otbNMvP5D1' });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
+
+
+// send simple notification
+function sendProactiveMessage(address) {
+    var msg = new builder.Message().address(address);
+    msg.text('Hello, this is a notification');
+    msg.textLocale('en-US');
+    bot.send(msg);
+}
+
+var savedAddress;
+server.post('/api/messages', connector.listen());
+
+// Do GET this endpoint to delivey a notification
+server.get('/api/CustomWebApi', (req, res, next) => {
+    sendProactiveMessage(savedAddress);
+res.send('triggered');
+next();
+}
+);
+
+bot.dialog('/', function(session, args) {
+
+    savedAddress = session.message.address;
+
+    var message = 'Hello! In a few seconds I\'ll send you a message proactively to demonstrate how bots can initiate messages.';
+    session.send(message);
+
+    message = 'You can also make me send a message by accessing: ';
+    message += 'http://localhost:' + server.address().port + '/api/CustomWebApi';
+    session.send(message);
+
+    setTimeout(() => {
+        sendProactiveMessage(savedAddress);
+}, 5000);
+});
+
+
+
 // Root dialog for entry point in application
-bot.dialog('/', [
+/*bot.dialog('/', [
     function (session,args, next) {
         session.send("Hi");
     }
 
-]);
+]);*/
+
+
 
